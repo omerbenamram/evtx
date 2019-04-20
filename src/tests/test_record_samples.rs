114245726aea52119b1c92c4f9c5245251b7d36c
Omer Ben-Amram <omerbenamram@gmail.com>
2019-04-20 18:20:45 +0300

    added another test

--- src/tests/test_record_samples.rs
+++ src/tests/test_record_samples.rs
@@ -1,11 +1,13 @@
-use crate::{ensure_env_logger_initialized, EvtxParser};
+use crate::{ensure_env_logger_initialized, EvtxParser, ParserSettings};
 use pretty_assertions::assert_eq;
 
 #[test]
 fn test_event_xml_sample() {
     ensure_env_logger_initialized();
     let evtx_file = include_bytes!("../../samples/security.evtx");
-    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec()).unwrap();
+    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec())
+        .unwrap()
+        .with_configuration(ParserSettings::new().num_threads(1));
 
     let first_record = parser
         .records()
@@ -30,7 +32,9 @@ fn test_event_xml_sample() {
 fn test_event_json_sample() {
     ensure_env_logger_initialized();
     let evtx_file = include_bytes!("../../samples/security.evtx");
-    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec()).unwrap();
+    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec())
+        .unwrap()
+        .with_configuration(ParserSettings::new().num_threads(1));
 
     let first_record = parser
         .records_json()
@@ -45,6 +49,60 @@ fn test_event_json_sample() {
             .map(|l| l.trim())
             .collect::<String>(),
         include_str!("../../samples/security_event_1.json")
+            .lines()
+            .map(str::trim)
+            .collect::<String>()
+    );
+}
+
+#[test]
+fn test_event_json_sample_with_event_data() {
+    ensure_env_logger_initialized();
+    let evtx_file = include_bytes!("../../samples/2-system-Security-dirty.evtx");
+    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec())
+        .unwrap()
+        .with_configuration(ParserSettings::new().num_threads(1));
+
+    let first_record = parser
+        .records_json()
+        .next()
+        .expect("to have records")
+        .expect("record to parse correctly");
+
+    assert_eq!(
+        first_record
+            .data
+            .lines()
+            .map(|l| l.trim())
+            .collect::<String>(),
+        include_str!("../../samples/event_with_eventdata.json")
+            .lines()
+            .map(str::trim)
+            .collect::<String>()
+    );
+}
+
+#[test]
+fn test_event_xml_sample_with_event_data() {
+    ensure_env_logger_initialized();
+    let evtx_file = include_bytes!("../../samples/2-system-Security-dirty.evtx");
+    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec())
+        .unwrap()
+        .with_configuration(ParserSettings::new().num_threads(1));
+
+    let first_record = parser
+        .records()
+        .next()
+        .expect("to have records")
+        .expect("record to parse correctly");
+
+    assert_eq!(
+        first_record
+            .data
+            .lines()
+            .map(|l| l.trim())
+            .collect::<String>(),
+        include_str!("../../samples/event_with_eventdata.xml")
             .lines()
             .map(str::trim)
             .collect::<String>()
