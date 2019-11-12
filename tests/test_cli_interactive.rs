1ebe579ae90de162d3195abe74fe22abfee4e44b
Omer Ben-Amram <omerbenamram@gmail.com>
2019-11-12 23:17:40 +0200

    scope interactive tests into modules to avoid confusing cargo fix

--- tests/test_cli_interactive.rs
+++ tests/test_cli_interactive.rs
@@ -1,75 +1,85 @@
-// The interactive tests are in a separate file,
-// since they use `rexpect`, which internally uses quirky fork semantics to open a pty.
-// They will fail if tried to be executed concurrently any other CLI test.
-
+/// The interactive tests are in a separate file,
+/// since they use `rexpect`, which internally uses quirky fork semantics to open a pty.
+/// They will fail if tried to be executed concurrently any other CLI test.
 mod fixtures;
 
+#[cfg(target_os = "windows")]
+mod tests {}
+
 #[cfg(not(target_os = "windows"))]
-use rexpect::spawn;
+mod tests {
+    use super::fixtures::*;
 
-// It should behave the same on windows, but interactive testing relies on unix pty internals.
-#[test]
-#[cfg(not(target_os = "windows"))]
-fn test_it_confirms_before_overwriting_a_file() {
-    let d = tempdir().unwrap();
-    let f = d.as_ref().join("test.out");
+    use assert_cmd::cargo::cargo_bin;
+    use rexpect::spawn;
+    use std::fs::File;
+    use std::io::{Read, Write};
+    use tempfile::tempdir;
 
-    let mut file = File::create(&f).unwrap();
-    file.write_all(b"I'm a file!").unwrap();
+    // It should behave the same on windows, but interactive testing relies on unix pty internals.
+    #[test]
+    #[cfg(not(target_os = "windows"))]
+    fn test_it_confirms_before_overwriting_a_file() {
+        let d = tempdir().unwrap();
+        let f = d.as_ref().join("test.out");
 
-    let sample = regular_sample();
+        let mut file = File::create(&f).unwrap();
+        file.write_all(b"I'm a file!").unwrap();
 
-    let cmd_string = format!(
-        "{bin} -f {output_file} {sample}",
-        bin = cargo_bin("evtx_dump").display(),
-        output_file = f.to_string_lossy(),
-        sample = sample.to_str().unwrap()
-    );
+        let sample = regular_sample();
 
-    let mut p = spawn(&cmd_string, Some(10000)).unwrap();
-    p.exp_regex(r#"Are you sure you want to override.*"#)
-        .unwrap();
-    p.send_line("y").unwrap();
-    p.exp_eof().unwrap();
+        let cmd_string = format!(
+            "{bin} -f {output_file} {sample}",
+            bin = cargo_bin("evtx_dump").display(),
+            output_file = f.to_string_lossy(),
+            sample = sample.to_str().unwrap()
+        );
 
-    let mut expected = vec![];
+        let mut p = spawn(&cmd_string, Some(10000)).unwrap();
+        p.exp_regex(r#"Are you sure you want to override.*"#)
+            .unwrap();
+        p.send_line("y").unwrap();
+        p.exp_eof().unwrap();
 
-    File::open(&f).unwrap().read_to_end(&mut expected).unwrap();
-    assert!(
-        !expected.len() > 100,
-        "Expected output to be printed to file"
-    )
-}
+        let mut expected = vec![];
 
-#[test]
-#[cfg(not(target_os = "windows"))]
-fn test_it_confirms_before_overwriting_a_file_and_quits() {
-    let d = tempdir().unwrap();
-    let f = d.as_ref().join("test.out");
+        File::open(&f).unwrap().read_to_end(&mut expected).unwrap();
+        assert!(
+            !expected.len() > 100,
+            "Expected output to be printed to file"
+        )
+    }
 
-    let mut file = File::create(&f).unwrap();
-    file.write_all(b"I'm a file!").unwrap();
+    #[test]
+    #[cfg(not(target_os = "windows"))]
+    fn test_it_confirms_before_overwriting_a_file_and_quits() {
+        let d = tempdir().unwrap();
+        let f = d.as_ref().join("test.out");
 
-    let sample = regular_sample();
+        let mut file = File::create(&f).unwrap();
+        file.write_all(b"I'm a file!").unwrap();
 
-    let cmd_string = format!(
-        "{bin} -f {output_file} {sample}",
-        bin = cargo_bin("evtx_dump").display(),
-        output_file = f.to_string_lossy(),
-        sample = sample.to_str().unwrap()
-    );
+        let sample = regular_sample();
 
-    let mut p = spawn(&cmd_string, Some(10000)).unwrap();
-    p.exp_regex(r#"Are you sure you want to override.*"#)
-        .unwrap();
-    p.send_line("n").unwrap();
-    p.exp_eof().unwrap();
+        let cmd_string = format!(
+            "{bin} -f {output_file} {sample}",
+            bin = cargo_bin("evtx_dump").display(),
+            output_file = f.to_string_lossy(),
+            sample = sample.to_str().unwrap()
+        );
 
-    let mut expected = vec![];
+        let mut p = spawn(&cmd_string, Some(10000)).unwrap();
+        p.exp_regex(r#"Are you sure you want to override.*"#)
+            .unwrap();
+        p.send_line("n").unwrap();
+        p.exp_eof().unwrap();
 
-    File::open(&f).unwrap().read_to_end(&mut expected).unwrap();
-    assert!(
-        !expected.len() > 100,
-        "Expected output to be printed to file"
-    )
+        let mut expected = vec![];
+
+        File::open(&f).unwrap().read_to_end(&mut expected).unwrap();
+        assert!(
+            !expected.len() > 100,
+            "Expected output to be printed to file"
+        )
+    }
 }
