9eba43990242470d8ff209f3207ad2dbdee1b6be
Omer Ben-Amram <omerbenamram@gmail.com>
2020-01-12 15:41:57 +0200

    removed a lot of dead code

--- src/binxml/name.rs
+++ src/binxml/name.rs
@@ -17,7 +17,6 @@ use std::fmt;
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct BinXmlName<'a> {
     str: Cow<'a, str>,
-    data_size: u32,
 }
 
 #[derive(Debug, PartialOrd, PartialEq, Clone)]
@@ -87,77 +86,22 @@ impl BinXmlNameRef {
 impl<'a> BinXmlName<'a> {
     #[cfg(test)]
     pub(crate) fn from_str(s: &'a str) -> Self {
-        let nul_terminator_sz = 4;
-
         BinXmlName {
             str: Cow::Borrowed(s),
-            data_size: (s.len() * 2 + nul_terminator_sz + BinXmlNameLink::data_size() as usize)
-                as u32,
         }
     }
 
     #[cfg(test)]
     pub(crate) fn from_string(s: String) -> Self {
-        BinXmlName {
-            str: Cow::Owned(s),
-            data_size: 0,
-        }
-    }
-
-    pub fn from_binxml_stream(
-        cursor: &mut Cursor<&'a [u8]>,
-        chunk: Option<&'a EvtxChunk<'a>>,
-    ) -> Result<Self> {
-        // Important!!
-        // The "offset_from_start" refers to the offset where the name struct begins.
-        let name_offset = try_read!(cursor, u32, "name_offset")?;
-
-        // If name is cached, read it and seek ahead if needed.
-        if let Some(name) =
-            chunk.and_then(|chunk| chunk.string_cache.get_cached_string(name_offset))
-        {
-            // Seek if needed
-            if cursor.position() == u64::from(name_offset) {
-                let position_after_string = cursor.position() + u64::from(name.data_size);
-                try_seek!(cursor, position_after_string, "Skip cached string")?;
-            }
-
-            // This doesn't actually clone the string, just the reference and the `data_size`.
-            Ok(name.clone())
-        } else {
-            let current_position = cursor.position();
-
-            if current_position != u64::from(name_offset) {
-                try_seek!(cursor, name_offset, "Seek to string")?;
-
-                let _ = BinXmlNameLink::from_stream(cursor)?;
-                let ret = Self::from_stream(cursor);
-
-                try_seek!(cursor, current_position, "Seek back after reading string")?;
-                ret
-            } else {
-                trace!("name is here");
-                let _ = BinXmlNameLink::from_stream(cursor)?;
-                Self::from_stream(cursor)
-            }
-        }
+        BinXmlName { str: Cow::Owned(s) }
     }
 
     /// Reads a tuple of (String, Hash, Offset) from a stream.
     pub fn from_stream(cursor: &mut Cursor<&'a [u8]>) -> Result<Self> {
-        let position_before_read = cursor.position();
-
         let name = try_read!(cursor, len_prefixed_utf_16_str_nul_terminated, "name")?
             .unwrap_or(Cow::Borrowed(""));
 
-        let position_after_read = cursor.position();
-        let data_size = (position_after_read - position_before_read) as ChunkOffset
-            + BinXmlNameLink::data_size();
-
-        Ok(BinXmlName {
-            str: name,
-            data_size,
-        })
+        Ok(BinXmlName { str: name })
     }
 
     pub fn as_str(&self) -> &str {
