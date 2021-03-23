938d9e6190c122389cfbd5dc12c9a7b5e7add193
Omer Ben-Amram <omerbenamram@gmail.com>
2021-03-23 11:00:32 +0200

    Added github actions release infrastructure
    - Support static linux binaries.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![deny(unused_must_use)]
 #![cfg_attr(backtraces, feature(backtrace))]
 #![forbid(unsafe_code)]
+#![allow(clippy::upper_case_acronyms)]
 // Don't allow dbg! prints in release.
 #![cfg_attr(not(debug_assertions), deny(clippy::dbg_macro))]
 // This needs to come first!
