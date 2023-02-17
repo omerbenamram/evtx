e6ac3a83f83128c36659cba4a8809ae79fcd5e63
Alex Kornitzer <a.kornitzer@me.com>
2023-02-17 10:27:27 +0000

    feat: add initial support for size_t
    From looking at an event log which utilises size_t it can be seen to
    output size_t in hex. This commit adds support for widths 4 & 8, by
    applying them to the HexInt32 and HexInt64 variants.

--- tests/test_record_samples.rs
+++ tests/test_record_samples.rs
@@ -167,6 +167,24 @@ fn test_event_xml_sample_with_entity_ref_2() {
 }
 
 #[test]
+fn test_event_json_with_size_t() {
+    ensure_env_logger_initialized();
+    let evtx_file = include_bytes!("../samples/Security_with_size_t.evtx");
+    let mut parser = EvtxParser::from_buffer(evtx_file.to_vec())
+        .unwrap()
+        .with_configuration(ParserSettings::new().num_threads(1));
+
+    let record = parser
+        .records_json()
+        .filter_map(|record| record.ok())
+        .find(|record| record.event_record_id == 196)
+        .expect("record to parse correctly");
+
+    let value: Value = serde_json::from_str(&record.data).expect("to parse correctly");
+    insta::assert_json_snapshot!(&value);
+}
+
+#[test]
 fn test_event_json_with_multiple_nodes_same_name() {
     ensure_env_logger_initialized();
     let evtx_file = include_bytes!(
